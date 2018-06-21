98578220776640abd0af57aa35ed1ec9e399f25c
Zuqiang Ke <jack_ke@ymail.com>
2018-06-21 15:28:11 -0500

    Initial Step

--- BasicProject/app.py
+++ BasicProject/app.py
@@ -9,8 +9,18 @@ app = Flask(__name__)
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
 
+# you can use as many decorators as you want if the same function serves multiple routes
+# The variable can be part of the URL path or in a query parameter. For example, a route 
+# in the form of '/hello/<name> generates a string argument called name to the function, 
+# and using ?message=<msg> in the route parses the value given for the "message=" query 
+# parameter and passes it to the function as msg
+
+#@app.route('/hello/<name>?message=<msg>')
+#def hello(name, msg):
+#    return "Hello " + name + "! Message is " + msg + "."
 
 @app.route('/')
+@app.route('/hello')
 def hello():
     """Renders a sample page."""
     return "Hello World!"
