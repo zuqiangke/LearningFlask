e2964da884efe03065a185bf7798e9fc530e65a8
Zuqiang Ke <jack_ke@ymail.com>
2018-06-21 17:11:33 -0500

    Save static files

--- BasicProject/HelloFlask/views.py
+++ BasicProject/HelloFlask/views.py
@@ -14,3 +14,11 @@ def home():
    # return html_content
    # return render_template("index.html", content = "<strong>Hello Flask</strong> on " + formatted_now) # render_template to load the template and supply a value for "content", which is done using a named argument matching the name of the placeholder. Flask automatically looks for templates in the templates folder
     return render_template("index.html", title="Hello Flask", message="Hello, Flask !", content = "on" + formatted_now)
+
+@app.route('/api/data')
+def get_data():
+    return app.send_static_file('data.json')
+
+@app.route('/about')
+def about():
+    return render_template("about.html",title="About HelloFlask",content="Example app page for Flask")
